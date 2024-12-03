a513dc86cddcec08cb5284ff00597530b11b089a
SezerGuvener <75906653+SezerGuvener@users.noreply.github.com>
2024-12-04 00:27:48 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,31 @@
+Config = {}
+
+Config.DrawDistance = 10.0
+Config.MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
+
+Config.Money = 0 -- How much it takes from you when you open the clothing menu
+Config.Debug = false
+
+Config.Stores = {
+    [1] = {shopType = 'clothing', coords = vector3(1693.32, 4823.48, 41.06), width = 2, length = 2}, -- Grapeseed
+    [2] = {shopType = 'clothing', coords = vector3(-709.77, -153.04, 37.42), width = 2, length = 2}, -- Portola Drive
+    [3] = {shopType = 'clothing', coords = vector3(-1193.18, -767.96, 17.32), width = 2, length = 2}, -- North Rockford Drive
+    [4] = {shopType = 'clothing', coords = vector3(425.236, -806.008, 28.491), width = 2, length = 2}, -- Sinner Street
+    [5] = {shopType = 'clothing', coords = vector3(-163.54, -303.16, 39.73), width = 2, length = 2}, -- Las Lagunas Blvd.
+    [6] = {shopType = 'clothing', coords = vector3(75.34, -1392.91, 29.38), width = 2, length = 2}, -- Innocence Blvd
+    [7] = {shopType = 'clothing', coords = vector3(-822.47, -1073.55, 11.33), width = 2, length = 2}, -- South Rockford Drive
+    [8] = {shopType = 'clothing', coords = vector3(-1450.53, -237.1, 49.81), width = 2, length = 2}, -- Cougar Ave.
+    [9] = {shopType = 'clothing', coords = vector3(4.61, 6512.48, 31.88), width = 2, length = 2}, -- Paleto Bay
+    [10] = {shopType = 'clothing', coords = vector3(614.27, 2762.81, 42.09), width = 2, length = 2}, -- Grapeseed 2
+    [11] = {shopType = 'clothing', coords = vector3(1196.8, 2710.06, 38.22), width = 2, length = 2}, -- Harmony
+    [12] = {shopType = 'clothing', coords = vector3(-3170.81, 1043.6, 20.86), width = 2, length = 2}, -- Great Ocean Highway
+    [13] = {shopType = 'clothing', coords = vector3(-1101.26, 2710.46, 19.11), width = 2, length = 2}, -- Route 68
+    [14] = {shopType = 'clothing', coords = vector3(125.52, -223.72, 54.56), width = 2, length = 2}, -- Hawick Ave.
+    [15] = {shopType = 'barber', coords = vector3(-814.32, -183.77, 37.57), width = 2, length = 2}, -- Mad Wayne Thunder Drive
+    [16] = {shopType = 'barber', coords = vector3(137.04, -1708.0, 29.29), width = 2, length = 2}, -- Carson Ave.
+    [17] = {shopType = 'barber', coords = vector3(-1282.49, -1117.22, 6.99), width = 2, length = 2}, -- Magellan Ave.
+    [18] = {shopType = 'barber', coords = vector3(1931.58, 3730.48, 32.84), width = 2, length = 2}, -- Sandy Shores
+    [19] = {shopType = 'barber', coords = vector3(1212.67, -472.74, 66.21), width = 2, length = 2}, -- Mirror Park
+    [20] = {shopType = 'barber', coords = vector3(-32.92, -152.78, 57.08), width = 2, length = 2}, -- Hawick Ave. 
+    [21] = {shopType = 'barber', coords = vector3(-278.04, 6228.5, 31.7), width = 2, length = 2} -- Paleto Bay
+}
